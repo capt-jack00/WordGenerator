2f87572676ddcc65e48a6fc2a4722e66614dfadc
Tytus <dbednorz@tuta.io>
2026-01-20 18:31:08 +0100

    Minor improvements
    Changed the infinite while loop to for loop
    Now every randomly chosen tile is saved in terrain array

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use std::{thread, time};
 fn main() {
     let mut rng = rand::rng();
     let tiles: [char; 3] = ['g', 'w', 's'];
+    let mut terrain: [char; 10] = ['-'; 10];
     let max = 2;
-    while true{
-        println!("Random tile: {}", tiles[rng.random_range(0..max)]);
-        thread::sleep(time::Duration::from_millis(500));
+
+    for i in 0..9{
+        let mut ranum = rng.random_range(0..max);
+        terrain[i] = tiles[ranum];
+        println!("Random tile: {}", tiles[ranum]);
+        thread::sleep(time::Duration::from_millis(500));                            //Sleep time in milliseconds to increase readabilty
+        ranum = 0;
     }
 }
