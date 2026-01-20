5d1d86e4cb259a98ff1b2a12fba81553d1cd3790
Tytus <dbednorz@tuta.io>
2026-01-20 18:16:48 +0100

    Added tiles generation
    Array tiles has been added
    The program chooses a random number between 0 and 2 which is an equivalent to array position.
    g is a representation of grass
    s is a representation for sand
    w is a representation for water
    
    This is nothing complicated for now because I'm just starting my journey with Rust language. I'm 100% sure that I want to finish this project fully in Rust tho.
    
    This Rust language seems very fun for now. It has features I haven't seen before. It's an amazing feeling to discover something new.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rand::Rng;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::rng();
+    let tiles: [char; 3] = ['g', 'w', 's'];
+    let max = 2;
+    while true{
+        println!("Random tile: {}", tiles[rng.random_range(0..max)]);
+        thread::sleep(time::Duration::from_millis(500));
+    }
 }
