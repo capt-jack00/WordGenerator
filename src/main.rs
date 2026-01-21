2fb6ec2d41ee433ea30f39e26c5e95367088aaa2
Tytus <dbednorz@tuta.io>
2026-01-21 19:02:19 +0100

    Major code improvements
    Changes:
    - Improved variable names
    - Removed terrain array and added terrain_map
    - Improved the one loop that randomly generates tiles
    - Added a loop that prints out the ready terrain
    
    Like I said, Rust is something new for me and it's sometimes confusing for someone that used to code in C++, C#, Java and Python for the entire time.
    
    For now the thing that confuses me the most is looping through an array because, it's a bit weird when after you do for e in arr you no longer can access or edit the arr because it was taken away from you.
    
    I don't really see any use for that mechanic just yet, but I believe it may be very powerful.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 use rand::Rng;
-use std::{thread, time};
 
 fn main() {
-    let mut rng = rand::rng();                                                      //Random engine initialization
+    let mut rng_engine = rand::rng();                                                      //Random engine initialization
     let tiles: [char; 3] = ['g', 'w', 's'];                                         //Array with tiles, each char represents a tile. In this case: g = grass, w = water and s = sand
-    let mut terrain: [char; 10] = ['-'; 10];                                        //Terrain array with max capacity of 10 elements. At first it's filled with '-' and as the program runs it's getting filled with chars representing tiles
-    let max = 2;                                                                    //Max random range
+    let max_ran_range= 2;                                                                //Max random range
 
-    for i in 0..9{
-        let mut ranum = rng.random_range(0..max);                                   //Generates a random number in range between 0 and max 
-        terrain[i] = tiles[ranum];                                                  //Saves the generated number to terrain array on position i
-        println!("Random tile: {}", tiles[ranum]);                                  
-        thread::sleep(time::Duration::from_millis(500));                            //Sleep time in milliseconds to increase readabilty
-        ranum = 0;                                                                  //Zeroing the ranum, just in case
+    let mut terrain_map: [[char; 3]; 3] = [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']
+    ];
+
+    for y in 0..terrain_map.len() {
+        for x in 0..terrain_map[y].len() {
+            let ranum = rng_engine.random_range(0..max_ran_range);
+            terrain_map[y][x] = tiles[ranum];
+        }
     }
+
+
+    println!("Generated terrain: ");    
+    for row in &terrain_map {
+        for tile in row {
+            println!("Tile: {}", tile);
+        }
+    }
+
 }
